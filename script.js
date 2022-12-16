c62be3f4dffcfcd80fe3d4fa7824b9827a788c51
Emma Gerigscott <elgerig@gmail.com>
2022-12-16 12:51:30 -0500

    generate computer choice

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-const computerChoice = document.getElementById('computer-choice')
+const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('.choice')
 
 let userChoice
+let computerChoice
 
 possibleChoices.forEach(choice => choice.addEventListener('click', (e) => {
     userChoice = e.target.id
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor((Math.random() * possibleChoices.length)+1)
+
+    switch (randomNumber) {
+        case 1:
+            computerChoice = 'rock'
+            break;
+        case 2:
+            computerChoice = 'paper'
+            break;
+        case 3:
+            computerChoice = 'scissors'
+            break;
+    }
+    computerChoiceDisplay.innerHTML = computerChoice
+}
